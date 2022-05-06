41dc5ed35614becd6e53d89aa959d90aa272f853
limjaeseong <22100619@handong.ac.kr>
2022-05-06 23:12:18 +0900

    add updateProduct

--- product.c
+++ product.c
@@ -28,3 +28,22 @@ void readProduct(Product s){
     printf("카테고리: %s\n",s.category);
     printf("제품 가격: %d\n",s.price); 
 }
+int updateProduct(Product *s){
+    printf("제품명은?: ");
+    getchar();
+    scanf("%[^\n]s", s->name);
+    printf("제품 설명은?: ");
+    getchar();
+    scanf("%[^\n]s", s->explain);
+    printf("무게는?: ");
+    scanf("%s",s->weight);
+    printf("유통기한은?(ex:04.29): ");
+    scanf("%s",s->date);
+    printf("제품 카테고리?: ");
+    getchar();
+    scanf("%s",s->category);
+    printf("판매 가격은?: ");
+    scanf("%d",&s->price);
+    printf("=>수정됨!\n");
+    return 1;
+}

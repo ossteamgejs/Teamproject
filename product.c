a401660e05652cccd5d3e779a9b148c0bc0ceeec
limjaeseong <22100619@handong.ac.kr>
2022-05-13 22:20:23 +0900

    modify main

--- product.c
+++ product.c
@@ -204,32 +204,70 @@ int main(void){
     while(1){
         menu=selectMenu();
         if(menu==0)break;
-        if(menu==1){
-        if(count>0){
-            readProduct(s);
+	if(menu == 1 || menu == 3 || menu == 4){
+        	if(count == 0) continue;
         }
-        else{
-            printf("데이터가 없습니다!\n");
-        }
+	if(menu==1){
+        	if(count>0){
+        		listProduct(slist, curcount);
+        	}else{
+        		printf("데이터가 없습니다!\n");
+        	}
         }
         else if(menu==2){
-        count+=addProduct(&s);
+        	count+=addProduct(&slist[curcount++]);
+		printf("=>추가됨!\n");
         }
         else if(menu==3){
-            updateProduct(&s);
+		int no = selectDataNo(slist, curcount);
+                        if(no == 0){
+                                printf("=>취소됨!\n");
+                                continue;
+                        }
+                        updateProduct(&slist[no-1]);
+
         }
         else if(menu==4){
-            if(deleteProduct(&s)){
-             count--;
-         }
-	printf("=>삭제됨!\n");
+		int no = selectDataNo(slist,curcount);
+                        if(no == 0){
+                                printf("=>취소됨!\n");
+                                continue;
+                        }
+                        int del;
+                        printf("삭제하시겠습니까?(삭제:1)");
+                        scanf("%d",&del);
+                        if (del == 1){
+                                if(deleteProduct(&psist[no-1])) count--;
+                                printf("=> 삭제됨!\n");
         }
+	else if(menu==5){
+		saveData(slist, curcount);
+	}
 	else if(menu==6){
 		if(count==0)
 			printf("=>데이터 없음!\n");
 		else
 			searchName(slist,curcount);
 	}
+	else if(menu==7){
+		if(count==0)
+			printf("=>데이터 없음!\n");
+		else
+			searchPrice(slist,curcount);
+	}
+	else if(menu==8){
+		if(count==0)
+			printf("=>데이터 없음!\n");
+		else
+			searchWeight(slist,curcount);
+	}
+	else if(menu==9){
+		if(count==0)
+			printf("=>데이터 없음!\n");
+		else
+			searchCategory(slist,curcount);
+	}
+	count++;
      }
     printf("종료됨!\n");
     return 0;
